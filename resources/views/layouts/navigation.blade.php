ce227b8f08a566bab864cf16da3e595219233f6f
Agasi Gilang Persada <464516p@gmail.com>
2024-12-04 13:12:30 +0700

    Add link for menu deposit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('deposits.index')" :active="request()->routeIs('deposits.index')">
+                        Deposit
+                    </x-nav-link>
                 </div>
             </div>
 
