e914f89a6fb913fd5bcb60d0d9f31a70149bf168
Agasi Gilang Persada <464516p@gmail.com>
2024-12-04 13:13:47 +0700

    Ganti nama default seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,7 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Agasi Gilang Persada',
             'email' => 'test@example.com',
         ]);
     }
